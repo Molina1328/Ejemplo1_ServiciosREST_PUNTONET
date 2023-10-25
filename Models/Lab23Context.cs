d781e1372d21e6577ea0fb0a3878d591cfe5ffae
Molina1328 <joelmolinavelez21@gmail.com>
2023-10-24 23:26:23 -0500

    Subida del proyecto en .NET referente a la práctica de servicios REST

---
+++ Models/Lab23Context.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjemploREST202251.Models;
+
+public partial class Lab23Context : DbContext
+{
+    public Lab23Context()
+    {
+    }
+
+    public Lab23Context(DbContextOptions<Lab23Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Lab23;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Empleado__3214EC07EBCC6776");
+
+            entity.ToTable("Empleado");
+
+            entity.Property(e => e.Departamento)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("departamento");
+            entity.Property(e => e.Edad).HasColumnName("edad");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Sueldo).HasColumnName("sueldo");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
